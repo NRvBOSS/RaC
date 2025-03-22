aa32bf4580bf8a26e5ab17d728385bb2a15fe58a
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-03-22 14:15:36 +0400

    image carousel created

--- src/views/AppCarousel.vue
+++ src/views/AppCarousel.vue
@@ -1,2 +1,69 @@
 <template>
-</template>
+    <swiper
+      :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+      }"
+      :speed="600"
+      :parallax="true"
+      :pagination="false"
+      :navigation="true"
+      :modules="modules"
+      class="w-full h-screen"
+    >
+      <swiper-slide
+        v-for="(slide, index) in slides"
+        :key="index"
+        class="relative flex flex-col justify-center items-center text-white p-10 bg-cover bg-center"
+        :style="{ 'background-image': `url(${slide.image})` }"
+      >
+        <!-- Overlay -->
+        <div class="absolute inset-0 bg-black/50"></div>
+  
+        <!-- Text Content -->
+        <div
+          class="relative text-center max-w-2xl text-2xl md:text-3xl font-bold drop-shadow-lg"
+          data-swiper-parallax="-200"
+        >
+          {{ slide.text }}
+        </div>
+      </swiper-slide>
+    </swiper>
+  </template>
+  
+  <script>
+  import { Swiper, SwiperSlide } from "swiper/vue";
+  import "swiper/css";
+  import "swiper/css/pagination";
+  import "swiper/css/navigation";
+  import { Parallax, Pagination, Navigation } from "swiper/modules";
+  
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const slides = [
+        {
+          image: "https://etimg.etb2bimg.com/photo/88048208.cms",
+          text: "Sürətli və etibarlı avtomobil icarəsi – İstədiyiniz maşını seçin, dərhal yola düşün!",
+        },
+        {
+          image: "https://advertising.expedia.com/wp-content/uploads/2020/08/Car-Hero_1920x800.jpg",
+          text: "Ən yaxşı qiymətlərlə avtomobil satışı və icarəsi – İndi alın, rahat sürün!",
+        },
+        {
+          image: "https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuJTIwaW4lMjBjYXJ8ZW58MHx8MHx8fDA%3D",
+          text: "RaC ilə arzuladığınız avtomobil bir toxunuş qədər yaxın!",
+        },
+      ];
+  
+      return {
+        modules: [Parallax, Pagination, Navigation],
+        slides,
+      };
+    },
+  };
+  </script>
+  
--- src/views/AppHeader.vue
+++ src/views/AppHeader.vue
@@ -4,8 +4,8 @@
       <p class="text-blue-500">RaC</p>
     </a>
     <textarea
-      class="border-2 border-white hover:border-blue-500 focus:border-blue-500 focus:outline-none w-[35rem] max-w-full rounded-2xl py-2 px-4 resize-none bg-white transition-colors duration-300"
-      placeholder="Search"
+      class="border-2 border-white hover:border-blue-500 focus:border-blue-500 focus:outline-none w-[35rem] max-w-full rounded-2xl content-around px-4 resize-none bg-white transition-colors duration"
+      placeholder="Search🔍"
     ></textarea>
     <a
       class="mr-8 rounded-2xl border-2 border-blue-500 bg-white hover:bg-blue-500 hover:text-white transition-colors duration-300"
