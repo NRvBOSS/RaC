e60f5cd2c7782f7a35da85d1ab2f58fc6cd0d459
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-03-26 21:22:22 +0400

    add typing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export const theme = {
-    extend: {
+module.exports = {
+    theme: {
+      extend: {
         keyframes: {
-            typing: {
-                "0%": {
-                    width: "0%",
-                    opacity: "0"
-                },
-                "10%": {
-                    opacity: "1"
-                },
-                "100%": {
-                    width: "100%"
-                }
-            },
-            blink: {
-                "50%": {
-                    borderColor: "transparent"
-                },
-                "100%": {
-                    borderColor: "white"
-                }
-            }
+          typing: {
+            '0%': { width: '0ch' }, // Mətnin genişliyi 0 simvol
+            '100%': { width: '22ch' } // Mətnin simvol sayı qədər açılır
+          },
+          blink: {
+            '50%': { 'border-color': 'transparent' }
+          }
         },
         animation: {
-            typing: "typing 2s steps(20, end) infinite alternate",
-            blink: "blink .7s infinite"
+          typing: 'typing 3s steps(22, end) forwards',
+          blink: 'blink 0.7s infinite'
         }
-    },
-};
-export const plugins = [];
+      }
+    }
+  }
+  
