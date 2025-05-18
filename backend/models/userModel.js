5135450c1024d526cba924f8119fd9bd79d4c1ae
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-05-18 23:41:34 +0400

    fix bug

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,33 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address']
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    // select: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
